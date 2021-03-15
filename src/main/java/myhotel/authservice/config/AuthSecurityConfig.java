36f149ac05ae1527c8914682d2bb64a68cf3f295
Setup <Setup@EPINHYDW07CA.princeton.epam.com>
2021-03-15 10:40:17 +0530

    enabled swagger openAPI documentation

--- src/main/java/myhotel/authservice/config/AuthSecurityConfig.java
+++ src/main/java/myhotel/authservice/config/AuthSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -39,10 +40,14 @@ public class AuthSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.cors().and().csrf().disable().
-				authorizeRequests().antMatchers("/authenticate").permitAll()
-				.anyRequest()
+		http.cors().and().csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll().anyRequest()
 				.authenticated();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/v2/api-docs", "/v3/api-docs/**", "/configuration/ui", "/swagger-resources/**",
+				"/configuration/security", "/swagger-ui.html", "/webjars/**", "/swagger-ui/**");
 	}
 
 }
